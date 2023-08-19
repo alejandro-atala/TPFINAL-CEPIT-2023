5bda1732ac8e88f2d76f23f7c6edad4d8c7a0b22
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-08-19 19:26:13 -0300

    link asistencia

--- Front/src/PagAlumno.js
+++ Front/src/PagAlumno.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import Notas from './Notas/notas';
-import Boletin from './Boletin/boletin';
-import Materias from './Materias/materias';
-import Asistencia from './Asistencia/asistencia';
-import Avisos from './Avisos/avisos';
-import Mensaje from './Mensaje/mensaje';
-import SideMenu from './MenuLateral/sideMenu';
-import Alumno from './Alumno/alumno';
-import { Route, Routes } from 'react-router-dom';
+
 
 function PagAlumno() {
   return (
     <div>
-      <Alumno />
-      <SideMenu />
-      <Routes>
-        {/* Rutas para las páginas */}
-        <Route path="/notas" element={<Notas />} />
-        <Route path="/boletin" element={<Boletin />} />
-        <Route path="/materias" element={<Materias />} />
-        <Route path="/asistencia" element={<Asistencia />} />
-        <Route path="/avisos" element={<Avisos />} />
-        <Route path="/mensaje" element={<Mensaje />} />
-      </Routes>
+
+
+     
     </div>
   );
 }
