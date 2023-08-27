c007502a68c09e7e828b0f18a402944f442a0bc6
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-08-27 10:55:27 -0300

    clasNameName

--- Front/src/Alumno/alumno.js
+++ Front/src/Alumno/alumno.js
@@ -8,11 +8,11 @@ const Alumno = () => {
     <div className="row row-cols-1 row-cols-md-3 g-4 ">
         <div className="col ">
           <div className="card h-100">
-          <img src="https://images4.imagebam.com/ef/e7/3b/MENRF6F_o.jpg" classNameName="card-img-top" alt="..." />
+          <img src="https://images4.imagebam.com/ef/e7/3b/MENRF6F_o.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Notas de examenes</h5>
               <p className="card-text">Aqui podras ver las notas de los examenes realizados</p>
-              <Link to="/notas" classNameName="btn btn-primary" id="myButton">Ver notas</Link>
+              <Link to="/notas" className="btn btn-primary" id="myButton">Ver notas</Link>
             </div>
             <div className="card-footer">
               <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
@@ -22,11 +22,11 @@ const Alumno = () => {
         </div>
         <div className="col">
           <div className="card h-100">
-          <img src="https://images4.imagebam.com/75/42/5c/MENRF62_o.jpg" classNameName="card-img-top" alt="..." />
+          <img src="https://images4.imagebam.com/75/42/5c/MENRF62_o.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Boletin</h5>
               <p className="card-text">Aqui podras ver el boletin </p>
-              <Link to="/boletin" classNameName="btn btn-primary" id="myButton">Ver Boletin</Link>
+              <Link to="/boletin" className="btn btn-primary" id="myButton">Ver Boletin</Link>
             </div>
             <div className="card-footer">
               <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
@@ -35,11 +35,11 @@ const Alumno = () => {
         </div>
         <div className="col">
           <div className="card h-100">
-          <img src="https://images4.imagebam.com/08/79/bf/MENRF6G_o.jpg" classNameName="card-img-top" alt="..." />
+          <img src="https://images4.imagebam.com/08/79/bf/MENRF6G_o.jpg" className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Materias del año en curso</h5>
               <p className="card-text">Aqui veras las meterias con sus dias y horarios</p>
-              <Link to="/materias" classNameName="btn btn-primary" id="myButton">Ver Materias</Link>
+              <Link to="/materias" className="btn btn-primary" id="myButton">Ver Materias</Link>
             </div>
             <div className="card-footer">
               <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
@@ -49,11 +49,11 @@ const Alumno = () => {
 
         <div className="col">
             <div className="card h-100">
-            <img src="https://images4.imagebam.com/3e/e8/24/MENRF5Z_o.jpg" classNameName="card-img-top" alt="..." />
+            <img src="https://images4.imagebam.com/3e/e8/24/MENRF5Z_o.jpg" className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Asistencias</h5>
                 <p className="card-text">Aqui veras las asistencias. </p>
-                <Link to="/Asistencia" classNameName="btn btn-primary" id="myButton">Asistencias</Link>
+                <Link to="/Asistencia" className="btn btn-primary" id="myButton">Asistencias</Link>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
@@ -63,11 +63,11 @@ const Alumno = () => {
 
           <div className="col">
             <div className="card h-100">
-            <img src="https://images4.imagebam.com/96/d2/c0/MENRF61_o.jpg" classNameName="card-img-top" alt="..." />
+            <img src="https://images4.imagebam.com/96/d2/c0/MENRF61_o.jpg" className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Avisos de los profes</h5>
                 <p className="card-text">Aqui veras llos mensajes que envien los profesores a sus alumnos</p>
-                <Link to="/avisos" classNameName="btn btn-primary" id="myButton">Ver avisos</Link>
+                <Link to="/avisos" className="btn btn-primary" id="myButton">Ver avisos</Link>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
@@ -77,11 +77,11 @@ const Alumno = () => {
 
           <div className="col">
             <div className="card h-100">
-            <img src="https://images4.imagebam.com/d5/79/59/MENRF6I_o.jpg" classNameName="card-img-top" alt="..." />
+            <img src="https://images4.imagebam.com/d5/79/59/MENRF6I_o.jpg" className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Enviar mensaje</h5>
                 <p className="card-text">Aqui podras enviar un mensaje directamente al profesor</p>
-                <Link to="/mensaje" classNameName="btn btn-primary" id="myButton">Enviar Mensaje</Link>
+                <Link to="/mensaje" className="btn btn-primary" id="myButton">Enviar Mensaje</Link>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
