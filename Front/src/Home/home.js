75b7ed9e94737fd4da559aa0dbdbeb37b0af54ba
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-08-30 12:07:44 -0300

    css home

--- Front/src/Home/home.js
+++ Front/src/Home/home.js
@@ -1,60 +1,58 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import escuela1 from '../ImagenesDescargadas/ESCUELA2.jpg';
 import escuela2 from '../ImagenesDescargadas/ESCUELA.jpg';
 import escuela3 from '../ImagenesDescargadas/ESCUELA3.jpg';
-import './home.css'
-
+import './home.css';
 
 const HomePage = () => {
-
   return (
-    <div>
+    <div className="d-flex flex-column justify-content-center align-items-center min-vh-100">
     <h1>Instituto Secundario NN</h1>
     <div className="container plan-de-estudios mt-5">
       <div className="row">
         <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={escuela1} className="imgnormalizada d-block w-100" alt="foto de escuela" />
-              <div className="carousel-caption d-none d-md-block text-black">
+              <div className="carousel-item active">
+                <img src={escuela1} className="img-fluid d-block w-100" alt="foto de escuela" />
+                <div className="carousel-caption d-none d-md-block text-black">
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src={escuela2} className="img-fluid d-block w-100" alt="foto de escuela" />
+                <div className="carousel-caption d-none d-md-block">  
+                </div>
+              </div>
+              <div className="carousel-item">
+                <img src={escuela3} className="img-fluid d-block w-100" alt="foto de escuela" />
+                <div className="carousel-caption d-none d-md-block">
+                </div>
               </div>
             </div>
-            <div className="carousel-item">
-              <img src={escuela2} className="imgnormalizada d-block w-100" alt="foto de escuela" />
-              <div className="carousel-caption d-none d-md-block">  
-              </div>
-            </div>
-            <div className="carousel-item">
-              <img src={escuela3} className="imgnormalizada d-block w-100" alt="foto de escuela" />
-              <div className="carousel-caption d-none d-md-block">
-              </div>
-            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+          <h2>Estudia en nuestro Instituto</h2>
+          <p>¡Descubre un mundo de aprendizaje emocionante en nuestra escuela secundaria!
+             En nuestro campus, la curiosidad se convierte en conocimiento y las amistades perduran 
+             para toda la vida. Con un equipo de educadores apasionados y programas educativos de vanguardia,
+              estamos comprometidos a nutrir tus habilidades y potencial al máximo. 
+              Desde laboratorios científicos hasta actividades artísticas, 
+              deportes en equipo hasta proyectos comunitarios, encontrarás infinitas oportunidades 
+              para crecer y brillar. Únete a nuestra familia escolar y prepárate
+               para un emocionante viaje hacia el éxito académico y personal.
+                Tu futuro comienza aquí, en Nuestro Instituto. 
+                ¡Inscribe hoy y haz tus sueños realidad con nosotros!</p>
         </div>
-        <h2>Estudia en nuestro Instituto</h2>
-        <p>¡Descubre un mundo de aprendizaje emocionante en nuestra escuela secundaria!
-           En nuestro campus, la curiosidad se convierte en conocimiento y las amistades perduran 
-           para toda la vida. Con un equipo de educadores apasionados y programas educativos de vanguardia,
-            estamos comprometidos a nutrir tus habilidades y potencial al máximo. 
-            Desde laboratorios científicos hasta actividades artísticas, 
-            deportes en equipo hasta proyectos comunitarios, encontrarás infinitas oportunidades 
-            para crecer y brillar. Únete a nuestra familia escolar y prepárate
-             para un emocionante viaje hacia el éxito académico y personal.
-              Tu futuro comienza aquí, en Nuestro Instituto. 
-              ¡Inscribe hoy y haz tus sueños realidad con nosotros!</p>
       </div>
-    </div>
     </div>
   );
 };
 
 export default HomePage;
+
