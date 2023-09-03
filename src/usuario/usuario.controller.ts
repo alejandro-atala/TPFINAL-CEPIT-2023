0e7d336f4b8331dca613081bdccbcee114007a94
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-09-03 20:41:08 -0300

    pase la logica el servicio

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,90 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UnauthorizedException } from '@nestjs/common';
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { CredencialesDto } from './dto/credenciales.dto';
-import { Profesor } from 'src/profesor/entities/profesor.entity';
-import { Alumno } from 'src/alumno/entities/alumno.entity';
-
-
 
 @Controller('usuario')
 export class UsuarioController {
-  constructor(private readonly usuarioService: UsuarioService) { }
-
-
-
-
+  constructor(private readonly usuarioService: UsuarioService) {}
 
   @Post('login')
   async iniciarSesion(@Body() credenciales: CredencialesDto) {
     try {
-      const usuario = await this.usuarioService.buscarPorEmail(credenciales.email);
-
-      if (!usuario) {
-        throw new UnauthorizedException('Correo electrónico no registrado');
-      }
-
-      // Verificar la contraseña
-      const contraseñaCoincide = await this.usuarioService.verificarContraseña(credenciales);
-
-      if (!contraseñaCoincide) {
-        throw new UnauthorizedException('Contraseña incorrecta');
-      }
-
-      if (usuario.tipo !== 'Alumno' && usuario.tipo !== 'Profesor') {
-
-        throw new UnauthorizedException('Tipo de usuario no válido'); 
-      }
-
-      // Redirigir a diferentes rutas según el tipo de usuario
-      if (usuario.tipo === 'Alumno' || usuario.tipo === 'Profesor') {
-      
-        return { tipo: usuario.tipo, nombre: usuario.nombre, id: usuario.idUsuario };
-      }
-
-
-
+      const response = await this.usuarioService.iniciarSesion(credenciales);
+      return response;
     } catch (error) {
       throw new UnauthorizedException('Error al iniciar sesión');
     }
   }
 
-
   @Post()
   async createRegistro(@Body() createUsuarioDto: CreateUsuarioDto) {
     try {
-      const nuevoUsuario = await this.usuarioService.createUsuario(createUsuarioDto);
-      let usuarioAsociado;
-
-      // Verificar el tipo de usuario y asociarlo a la tabla correspondiente
-      if (createUsuarioDto.tipo === 'Alumno') {
-        const alumno = new Alumno();
-   
-        alumno.nombre = nuevoUsuario.nombre;
-        // alumno.idAlumno = nuevoUsuario.idUsuario;
-        alumno.curso = nuevoUsuario.curso;
-        alumno.usuarioId = nuevoUsuario.idUsuario;
-        usuarioAsociado = await this.usuarioService.asociarAlumno(alumno);
-      } 
-
-      else if (createUsuarioDto.tipo === 'Profesor') {
-
-        const profesor = new Profesor(); 
-
-
-        profesor.nombre = nuevoUsuario.nombre;
-      profesor.usuarioId = nuevoUsuario.idUsuario;
-
-        usuarioAsociado = await this.usuarioService.asociarProfesor(profesor);
- 
-      }
+      const usuarioAsociado = await this.usuarioService.createRegistro(
+        createUsuarioDto,
+      );
       return usuarioAsociado;
     } catch (error) {
-      // Lanza una excepción personalizada con un mensaje informativo
       throw new Error(`Error al crear el usuario: ${error.message}`);
-  // Otras rutas y controladores
+    }
+  }
 }
-
-}}
-  
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,101 +1,142 @@
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-import { Controller, Post, Body } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Usuario } from './entities/usuario.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CredencialesDto } from './dto/credenciales.dto';
 import { Alumno } from 'src/alumno/entities/alumno.entity';
 import { Profesor } from 'src/profesor/entities/profesor.entity';
-const saltRounds = 10; // Número de rondas de encriptación
+import { InjectRepository } from '@nestjs/typeorm';
 
+const saltRounds = 10; // Número de rondas de encriptación
 
 @Injectable()
 export class UsuarioService {
   constructor(
     @InjectRepository(Usuario)
     private usuarioRepository: Repository<Usuario>,
-    @InjectRepository(Profesor) 
-     private profesorRepository: Repository<Profesor>,
-     @InjectRepository(Alumno)
-     private alumnoRepository: Repository<Alumno>,
+    @InjectRepository(Profesor)
+    private profesorRepository: Repository<Profesor>,
+    @InjectRepository(Alumno)
+    private alumnoRepository: Repository<Alumno>,
   ) {}
 
+  async iniciarSesion(credenciales: CredencialesDto) {
+    try {
+      const usuario = await this.buscarPorEmail(credenciales.email);
 
+      if (!usuario) {
+        throw new UnauthorizedException('Correo electrónico no registrado');
+      }
+
+      // Verificar la contraseña
+      const contraseñaCoincide = await this.verificarContraseña(
+        credenciales,
+        usuario,
+      );
+
+      if (!contraseñaCoincide) {
+        throw new UnauthorizedException('Contraseña incorrecta');
+      }
+
+      if (usuario.tipo !== 'Alumno' && usuario.tipo !== 'Profesor') {
+        throw new UnauthorizedException('Tipo de usuario no válido');
+      }
+
+      // Redirigir a diferentes rutas según el tipo de usuario
+      if (usuario.tipo === 'Alumno' || usuario.tipo === 'Profesor') {
+        const token = await this.generateToken(usuario);
+        return {
+          tipo: usuario.tipo,
+          nombre: usuario.nombre,
+          id: usuario.idUsuario,
+          token,
+        };
+      }
+    } catch (error) {
+      throw new UnauthorizedException('Error al iniciar sesión');
+    }
+  }
+
+  async createRegistro(createUsuarioDto: CreateUsuarioDto) {
+    try {
+      const nuevoUsuario = await this.createUsuario(createUsuarioDto);
+      let usuarioAsociado;
+
+      // Verificar el tipo de usuario y asociarlo a la tabla correspondiente
+      if (createUsuarioDto.tipo === 'Alumno') {
+        usuarioAsociado = await this.asociarAlumno(nuevoUsuario, createUsuarioDto);
+      } else if (createUsuarioDto.tipo === 'Profesor') {
+        usuarioAsociado = await this.asociarProfesor(nuevoUsuario, createUsuarioDto);
+      }
+      return usuarioAsociado;
+    } catch (error) {
+      // Lanza una excepción personalizada con un mensaje informativo
+      throw new Error(`Error al crear el usuario: ${error.message}`);
+    }
+  }
 
   async createUsuario(createUsuarioDto: CreateUsuarioDto): Promise<Usuario> {
     const nuevoUsuario = this.usuarioRepository.create(createUsuarioDto);
-    nuevoUsuario.password = await bcrypt.hash(nuevoUsuario.password, saltRounds);
+    nuevoUsuario.password = await bcrypt.hash(
+      nuevoUsuario.password,
+      saltRounds,
+    );
 
-    // Asignar el tipo de usuario al nuevoUsuario
-
-    let usuarioAsociado: Usuario | null = null;
-
-    if (createUsuarioDto.tipo === 'Alumno') {
-      usuarioAsociado = await this.createAlumnoConUsuario(nuevoUsuario);
-    } else if (createUsuarioDto.tipo === 'Profesor') {
-      usuarioAsociado = await this.createProfesorConUsuario(nuevoUsuario);
-    }
-
-    return usuarioAsociado;
+    return this.usuarioRepository.save(nuevoUsuario);
   }
-
-  async createAlumnoConUsuario(usuario: Usuario): Promise<Usuario> {
-    // Validaciones y asignación de datos del alumno
-    // ...
-    return this.usuarioRepository.save(usuario);
-  }
-
-  async createProfesorConUsuario(usuario: Usuario): Promise<Usuario> {
-    // Validaciones y asignación de datos del profesor
-    // ...
-    return this.usuarioRepository.save(usuario);
-  }
-
 
   async buscarPorEmail(email: string): Promise<Usuario | null> {
     return this.usuarioRepository.findOne({ where: { email } });
   }
-  
 
-  async verificarContraseña(credenciales: CredencialesDto): Promise<Usuario | null> {
-    const usuario = await this.buscarPorEmail(credenciales.email);
-
+  async verificarContraseña(
+    credenciales: CredencialesDto,
+    usuario: Usuario,
+  ): Promise<boolean> {
     if (!usuario) {
       throw new NotFoundException('Usuario no encontrado');
     }
 
-    const contraseñaCoincide = await bcrypt.compare(credenciales.password, usuario.password); 
+    const contraseñaCoincide = await bcrypt.compare(
+      credenciales.password,
+      usuario.password,
+    );
 
     if (!contraseñaCoincide) {
       throw new NotFoundException('Contraseña incorrecta');
     }
 
-    
-    return usuario;
+    return true;
   }
 
+  async generateToken(user: Usuario): Promise<string> {
+    const payload = {
+      usuario: user.idUsuario, // User ID
+      email: user.email, // User email or any other relevant information
+    };
 
-async verificarTipoUsuario(credenciales: CredencialesDto, tipo: string): Promise<Usuario | null> {
-    const usuario = await this.verificarContraseña(credenciales);
+    // Implement your token generation logic here
+    // You may use the AuthService for this purpose
+    // Example: const token = await this.authService.generateToken(payload);
+    // Make sure to inject the AuthService and call its generateToken method
 
-    if (usuario.tipo !== tipo) {
-      throw new UnauthorizedException('Tipo de usuario incorrecto');
-    }
-
-    return usuario;
+    return 'your-token-here'; // Replace with the actual token generation logic
   }
 
-  
-  async asociarAlumno(alumno: Alumno): Promise<Alumno> {
-    return await this.alumnoRepository.save(alumno);
+  async asociarAlumno(usuario: Usuario, createUsuarioDto: CreateUsuarioDto) {
+    const alumno = new Alumno();
+    alumno.nombre = usuario.nombre;
+    // alumno.idAlumno = usuario.idUsuario;
+    alumno.curso = createUsuarioDto.curso;
+    alumno.usuarioId = usuario.idUsuario;
+    return this.alumnoRepository.save(alumno);
   }
 
-  async asociarProfesor(profesor: Profesor): Promise<Profesor> {
-    //console.log(profesor);
-    return await this.profesorRepository.save(profesor);
+  async asociarProfesor(usuario: Usuario, createUsuarioDto: CreateUsuarioDto) {
+    const profesor = new Profesor();
+    profesor.nombre = usuario.nombre;
+    profesor.usuarioId = usuario.idUsuario;
+    return this.profesorRepository.save(profesor);
   }
-  
 }
