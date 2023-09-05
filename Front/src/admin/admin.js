39c9c82a254d77c80869eb328e79c0d128fde6fd
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-09-05 16:26:08 -0300

    put y post

--- Front/src/admin/admin.js
+++ Front/src/admin/admin.js
@@ -16,6 +16,7 @@ const AdminPage = () => {
     usuario: {
       name: 'Usuarios',
       columns: [
+        { label: 'ID Usuario', key: 'idUsuario' },
         { label: 'Nombre', key: 'nombre' },
         { label: 'DNI', key: 'dni' },
         { label: 'F. Nacimiento', key: 'fechaNac' },
@@ -41,33 +42,25 @@ const AdminPage = () => {
         { label: 'ID Profesor', key: 'idProfesor' },
         { label: 'Nombre', key: 'nombre' },
         { label: 'Usuario ID', key: 'usuarioId' },
-
         // Agrega más columnas según tu tabla
       ],
-    }
-    ,
+    },
     curso: {
       name: 'Cursos',
       columns: [
         { label: 'ID Curso', key: 'idCurso' },
         { label: 'Año', key: 'anio' },
-       
-
         // Agrega más columnas según tu tabla
       ],
-    }
-    ,
+    },
     materias: {
       name: 'Materias',
       columns: [
         { label: 'ID Materia', key: 'idMateria' },
         { label: 'Materia', key: 'nombre' },
-
-
         // Agrega más columnas según tu tabla
       ],
-    }
- 
+    },
     // Agrega más tablas según tus necesidades
   };
 
@@ -75,7 +68,7 @@ const AdminPage = () => {
     const tableName = event.target.value;
     if (tableName) {
       try {
-        const response = await axios.get(`http://localhost:3000/${tableName}`); // Reemplaza con tu ruta de API
+        const response = await axios.get(`http://localhost:3000/${tableName}`);
         setTableData(response.data);
         setEditedData(response.data.slice()); // Copia de seguridad de los datos originales
         setSelectedTable(tableName);
@@ -89,9 +82,9 @@ const AdminPage = () => {
     }
   };
 
-  const handleCellEdit = (e, rowId, columnName) => {
-    const updatedData = editedData.map((row) => {
-      if (row.idUsuario === rowId) {
+  const handleCellEdit = (e, rowIndex, columnName) => {
+    const updatedData = editedData.map((row, index) => {
+      if (index === rowIndex) {
         return { ...row, [columnName]: e.target.innerText };
       }
       return row;
@@ -99,15 +92,31 @@ const AdminPage = () => {
     setEditedData(updatedData);
   };
 
-  const handleDeleteRow = (rowId) => {
-    const updatedData = editedData.filter((row) => row.id !== rowId);
+  const handleDeleteRow = (rowIndex) => {
+    const updatedData = editedData.filter((row, index) => index !== rowIndex);
     setEditedData(updatedData);
   };
 
-  const handleSaveChanges = async () => {
+  const handleAddRow = async () => {
     try {
-      // Envía los cambios al servidor (por ejemplo, mediante una solicitud PUT o POST)
-      await axios.put(`http://localhost:3000/${selectedTable}`, editedData); // Reemplaza con tu ruta de API
+      const newRow = {};
+      tableInfo[selectedTable].columns.forEach((column) => {
+        newRow[column.key] = ''; // Inicializa todas las columnas con valores vacíos
+      });
+
+      // Realiza una solicitud POST para agregar la nueva fila
+      const response = await axios.post(`http://localhost:3000/${selectedTable}`, newRow);
+      newRow.idUsuario = response.data.idUsuario; // Asigna el ID asignado por la base de datos
+      setEditedData([...editedData, newRow]);
+    } catch (error) {
+      console.error('Error al agregar una nueva fila:', error);
+    }
+  };
+
+  const handleSaveChanges = async (rowIndex) => {
+    try {
+      const id = editedData[rowIndex].idUsuario;
+      await axios.put(`http://localhost:3000/${selectedTable}/${id}`, editedData[rowIndex]);
       console.log('Cambios guardados con éxito.');
     } catch (error) {
       console.error('Error al guardar cambios:', error);
@@ -125,12 +134,11 @@ const AdminPage = () => {
           <h2>Selector de Tablas</h2>
           <select onChange={handleTableChange}>
             <option value="">Seleccionar tabla</option>
-            <option value="usuario">Usuarios</option>
-            <option value="alumno">Alumnos</option>
-            <option value="profesor">Profesores</option>
-            <option value="curso">Cursos</option>
-            <option value="materias">Materias</option>
-            {/* Agregar más opciones según tus tablas */}
+            {Object.keys(tableInfo).map((tableName) => (
+              <option key={tableName} value={tableName}>
+                {tableInfo[tableName].name}
+              </option>
+            ))}
           </select>
 
           {selectedTable && (
@@ -146,25 +154,37 @@ const AdminPage = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {editedData.map((row) => (
-                    <tr key={row.idUsuario}>
+                  {editedData.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
                       {tableInfo[selectedTable].columns.map((column) => (
                         <td
                           key={column.key}
                           contentEditable="true"
-                          onBlur={(e) => handleCellEdit(e, row.idUsuario, column.key)}
+                          onBlur={(e) => handleCellEdit(e, rowIndex, column.key)}
                         >
                           {row[column.key]}
                         </td>
                       ))}
                       <td>
-                        <button onClick={() => handleDeleteRow(row.idUsuario)}>Borrar</button>
+                        <button onClick={() => handleDeleteRow(rowIndex)}>Borrar</button>
                       </td>
                     </tr>
                   ))}
+                  {/* Agrega una fila adicional al final con campos de entrada vacíos */}
+                  <tr>
+                    {tableInfo[selectedTable].columns.map((column) => (
+                      <td key={column.key} contentEditable="true">
+                        {/* Campo de entrada vacío */}
+                      </td>
+                    ))}
+                    <td>
+                      <button onClick={handleAddRow}>Agregar</button>
+                    </td>
+                  </tr>
                 </tbody>
               </table>
-              <button onClick={handleSaveChanges}>Guardar Cambios</button>
+              {/* Agrega el botón "Guardar Cambios" para la fila agregada */}
+              <button onClick={() => handleSaveChanges(editedData.length - 1)}>Guardar Cambios</button>
             </div>
           )}
         </div>
