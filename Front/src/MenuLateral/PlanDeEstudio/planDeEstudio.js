5b79bae8f2fd4505d8f3b3401384e8db1981d49a
alejandro-atala <alejandro.atala@municipiodetandil.edu.ar>
2023-09-10 16:41:10 -0300

    plan de estudios, materias sin repetir

--- Front/src/MenuLateral/PlanDeEstudio/planDeEstudio.js
+++ Front/src/MenuLateral/PlanDeEstudio/planDeEstudio.js
@@ -4,24 +4,32 @@ import './planDeEstudios.css';
 import axios from 'axios';
 
 const PlanDeEstudios = () => {
-  const [materiasPorId, setMateriasPorId] = useState([]);
+  const [materiasPorCurso, setMateriasPorCurso] = useState({});
 
   useEffect(() => {
     const obtenerMateriasPorId = async (id) => {
       try {
         const response = await axios.get(`http://localhost:3000/materias-curso/${id}`);
-        return { id, materias: response.data.map((row) => row.materia) };
+        const materias = response.data.map((row) => row.materia);
+        return materias;
       } catch (error) {
         console.error(`Error al obtener datos del ID ${id}:`, error);
-        return { id, materias: [] };
+        return [];
       }
     };
 
     const obtenerMateriasDeIds = async () => {
       const ids = ["Primero", "Segundo", "Tercero", "Cuarto", "Quinto", "Sexto"];
-      const promesas = ids.map((id) => obtenerMateriasPorId(id));
-      const resultados = await Promise.all(promesas);
-      setMateriasPorId(resultados);
+      const materiasPorId = await Promise.all(ids.map((id) => obtenerMateriasPorId(id)));
+
+      const materiasPorCurso = {};
+      ids.forEach((id, index) => {
+        // Filtrar materias duplicadas en cada año usando un conjunto (Set)
+        const materiasUnicas = [...new Set(materiasPorId[index])];
+        materiasPorCurso[id] = materiasUnicas;
+      });
+
+      setMateriasPorCurso(materiasPorCurso);
     };
 
     obtenerMateriasDeIds();
@@ -31,14 +39,14 @@ const PlanDeEstudios = () => {
     <div className="container plan-de-estudios mt-5">
       <h1>Plan de Estudio Por Año</h1>
       <div className="row">
-        {materiasPorId.map((materiasPorId) => (
-          <div key={materiasPorId.id} className="col-md-6 col-lg-4 mb-4">
+        {Object.keys(materiasPorCurso).map((curso) => (
+          <div key={curso} className="col-md-6 col-lg-4 mb-4">
             <div className="card shadow">
               <div className="card-body">
-                <h2 className="card-title">{` ${materiasPorId.id}`}</h2>
+                <h2 className="card-title">{` ${curso}`}</h2>
                 <div className="materias">
-                  {materiasPorId.materias.map((materia, subIndex) => (
-                    <div key={subIndex} className="card-text">
+                  {materiasPorCurso[curso].map((materia, index) => (
+                    <div key={index} className="card-text">
                       {materia}
                     </div>
                   ))}
