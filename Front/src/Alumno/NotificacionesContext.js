f50252e5de2c2f0accb362f0ba4165456a460ba2
Your Name <silvio.alvarez@municipiodetandil.edu.ar>
2023-10-01 01:12:47 -0300

    veravisosporalumno

--- Front/src/Alumno/NotificacionesContext.js
+++ Front/src/Alumno/NotificacionesContext.js
@@ -1,3 +1,4 @@
+// NotificacionesContext.js
 import React, { createContext, useState, useContext } from 'react';
 
 export const NotificacionesContext = createContext();
@@ -6,7 +7,10 @@ export const NotificacionesProvider = ({ children }) => {
   const [notificaciones, setNotificaciones] = useState([]);
   const [unreadAvisosCount, setUnreadAvisosCount] = useState(0);
 
+  // Función para marcar los avisos como leídos
   const markAvisosComoLeidos = (notificacionId) => {
+    console.log("notificacionId:", notificacionId);
+    // Actualiza la lista de notificaciones marcando la que corresponde como leída
     const notificacionesActualizadas = notificaciones.map((notificacion) => {
       if (notificacion.id === notificacionId) {
         return { ...notificacion, leida: true };
@@ -14,8 +18,11 @@ export const NotificacionesProvider = ({ children }) => {
       return notificacion;
     });
 
+    // Actualiza el contador de avisos no leídos
     const unreadCount = notificacionesActualizadas.filter((aviso) => !aviso.leida).length;
     setUnreadAvisosCount(unreadCount);
+
+    // Actualiza la lista de notificaciones
     setNotificaciones(notificacionesActualizadas);
   };
 
