9f456857eea7a621b84212f3322b05677bd3dd25
VictoriaRodriguez22 <victoria.rodriguez@municipiodetandil.edu.ar>
2023-11-03 18:26:24 -0600

    termino vista de profe

--- Front/src/PaginaProfe/profesor.js
+++ Front/src/PaginaProfe/profesor.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import SessionExpiration from '../SesionExpired';
+import './profesor.css';
 
 
 const Profesor = () => {
@@ -8,34 +9,16 @@ const Profesor = () => {
     <>
           <div variant="danger" className="mt-3 text-center">
       <SessionExpiration />
-      {/* Resto de tu componente Profesor */}
     </div>
     <div className="container mt-4 p-5">
-
     <div className="row row-cols-1 row-cols-md-3 g-4 ">
-      
-        <div className="col ">
-          
+       <div className="col ">
           <div className="card h-100">
-          <img src="https://images4.imagebam.com/ef/e7/3b/MENRF6F_o.jpg" className="card-img-top" alt="..." />
+          <img src="https://images4.imagebam.com/ef/e7/3b/MENRF6F_o.jpg" className="img-profe" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Notas de examenes</h5>
-              <p className="card-text">Aqui podras cargar las notas de los examenes realizados</p>
-              <Link to="/Profnotas" className="btn btn-primary" id="myButton">Cargar Notas</Link>
-            </div>
-            <div className="card-footer">
-              <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
-             
-            </div>
-          </div>
-        </div>
-        <div className="col">
-          <div className="card h-100">
-          <img src="https://images4.imagebam.com/75/42/5c/MENRF62_o.jpg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Trimestre</h5>
-              <p className="card-text">Aqui podras cargar las notas trimestrales </p>
-              <Link to="/ProfTrimestre" className="btn btn-primary" id="myButton">Cargar Notas trimestre</Link>
+              <h5 className="card-title-profe">Notas de examenes</h5>
+              <p className="card-text-profe">Aqui podras cargar las notas de los examenes realizados</p>
+              <Link to="/Profnotas" className="btn btn-profe" id="myButton">Cargar Notas</Link>
             </div>
             <div className="card-footer">
               <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
@@ -44,61 +27,69 @@ const Profesor = () => {
         </div>
         <div className="col">
           <div className="card h-100">
-          <img src="https://images4.imagebam.com/08/79/bf/MENRF6G_o.jpg" className="card-img-top" alt="..." />
+          <img src="https://images4.imagebam.com/75/42/5c/MENRF62_o.jpg" className="img-profe" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">Materias del año en curso</h5>
-              <p className="card-text">Aqui veras las meterias con sus dias y horarios</p>
-              <Link to="/Profmaterias" className="btn btn-primary" id="myButton">Ver Materias de mis cursos</Link>
+              <h5 className="card-title-profe">Trimestre</h5>
+              <p className="card-text-profe">Aqui podrás cargar las notas trimestrales </p>
+              <Link to="/ProfTrimestre" className="btn btn-profe" id="myButton">Cargar Notas Trimestre</Link>
             </div>
             <div className="card-footer">
               <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
             </div>
           </div>
         </div>
-
+        <div className="col">
+          <div className="card h-100">
+          <img src="https://images4.imagebam.com/08/79/bf/MENRF6G_o.jpg" className="img-profe" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title-profe">Materias por Año</h5>
+              <p className="card-text-profe">Aqui verás las materias con sus días y horarios</p>
+              <Link to="/Profmaterias" className="btn btn-profe" id="myButton">Ver Materias de Mis Cursos</Link>
+            </div>
+            <div className="card-footer">
+              <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
+            </div>
+          </div>
+        </div>
         <div className="col">
             <div className="card h-100">
-            <img src="https://images4.imagebam.com/3e/e8/24/MENRF5Z_o.jpg" className="card-img-top" alt="..." />
+            <img src="https://images4.imagebam.com/3e/e8/24/MENRF5Z_o.jpg" className="img-profe" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Asistencias</h5>
-                <p className="card-text">Aqui veras las asistencias. </p>
-                <Link to="/ProfAsistencia" className="btn btn-primary" id="myButton">Cargar Asistencias</Link>
+                <h5 className="card-title-profe">Asistencias</h5>
+                <p className="card-text-profe">Aqui podrás cargar las asistencias de tus Alumnos </p>
+                <Link to="/ProfAsistencia" className="btn btn-profe" id="myButton">Cargar Asistencia</Link>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
               </div>
             </div>
           </div>
-
           <div className="col">
             <div className="card h-100">
-            <img src="https://images4.imagebam.com/96/d2/c0/MENRF61_o.jpg" className="card-img-top" alt="..." />
+            <img src="https://images4.imagebam.com/96/d2/c0/MENRF61_o.jpg" className="img-profe" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Avisos de los profes</h5>
-                <p className="card-text">Aqui veras llos mensajes que envien los profesores a sus alumnos</p>
-                <Link to="/Profavisos" className="btn btn-primary" id="myButton">Cargar avisos</Link>
+                <h5 className="card-title-profe">Avisos de los Profes</h5>
+                <p className="card-text-profe">Aqui podrás cargar avisos a tus Cursos</p>
+                <Link to="/Profavisos" className="btn btn-profe" id="myButton">Cargar Aviso</Link>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
               </div>
             </div>
           </div>
-
           <div className="col">
             <div className="card h-100">
-            <img src="https://images4.imagebam.com/d5/79/59/MENRF6I_o.jpg" className="card-img-top" alt="..." />
+            <img src="https://images4.imagebam.com/d5/79/59/MENRF6I_o.jpg" className="img-profe" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Enviar mensaje</h5>
-                <p className="card-text">Aqui podras enviar un mensaje directamente al profesor</p>
-                <Link to="/Profmensaje" className="btn btn-primary" id="myButton">Mensajes</Link>
+                <h5 className="card-title-profe">Enviar mensaje</h5>
+                <p className="card-text-profe">Aqui podrás enviar un mensaje directamente al Alumno</p>
+                <Link to="/Profmensaje" className="btn btn-profe" id="myButton">Mensajes</Link>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary" >Last updated <span id="elapsedTime"></span> mins ago</small>
               </div>
             </div>
           </div>
-
-
       </div>
     </div>
     </>
