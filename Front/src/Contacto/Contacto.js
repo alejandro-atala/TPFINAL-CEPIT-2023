21a5145a8585902f3526f459006b846d2ca0667b
Alejandro <alejandro.atala@municipiodetandil.edu.ar>
2023-11-12 07:38:49 -0300

    contacto

--- Front/src/Contacto/Contacto.js
+++ Front/src/Contacto/Contacto.js
@@ -95,7 +95,7 @@ const Contacto = () => {
   return (
     <div className="d-flex align-items-center justify-content-center" >
       <div className="mt-5 flex-column text-center" style={{ maxWidth: '800px' }}>
-        <div className="card card-custom">
+        <div className="card card-custom mx-auto">
           <div className="card-body">
             <h1 className="titulo-contacto card-title">¡Contáctanos!</h1>
             <p className="card-text">
