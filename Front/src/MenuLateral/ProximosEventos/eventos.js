ba7e25e34f0b2d898303d510758d892d55410192
Alejandro <alejandro.atala@municipiodetandil.edu.ar>
2023-11-20 10:01:58 -0300

    calendario

--- Front/src/MenuLateral/ProximosEventos/eventos.js
+++ Front/src/MenuLateral/ProximosEventos/eventos.js
@@ -25,46 +25,129 @@ const ProximosEventos = () => {
     obtenerTextoPorReferencia('Eventos', setTextoEventos);
   }, []);
 
+  const procesarDatosActosEventos = (datos) => {
+    const lineas = datos.split('\n');
+    return lineas.map((dato) => {
+      const [fecha, descripcion, ubicacion] = dato.split('<br>');
+      return {
+        fecha,
+        descripcion: `${descripcion}`, // Mantener solo la descripción y ubicación
+      };
+    });
+  };
+  
+  
+
+  // Llamada a la función para procesar los datos
+  const datosProcesados = procesarDatosActosEventos(textoActos);
+  const datosProcesados2 = procesarDatosActosEventos(textoEventos);
+
+
   const titulo = 'Eventos del Instituto';
 
   // Crear objetos con el HTML procesado
   const htmlProcesadoActos = { __html: textoActos };
   const htmlProcesadoEventos = { __html: textoEventos };
 
-  return (
-    <div>
-      <div className="row">
-        <div className="col-md-3 col-sm-6 col-xs-12">
-          <SideMenu />
-        </div>
 
-        <div className="col-md-6 a-proximos-eventos mt-5 text-center mx-auto">
-          <div className="row justify-content-center w-100">
-            <div className="text-center">
-              <h1 className='titulo-evento'>{titulo}</h1>
+  const eventosExtraidos = datosProcesados.map((evento) => ({
+    fecha: evento.fecha,
+    descripcion: evento.descripcion,
+    ubicacion: evento.ubicacion,
+  }));
+
+  
+  const eventosExtraidos2 = datosProcesados2.map((evento) => ({
+    fecha2: evento.fecha,
+    descripcion2: evento.descripcion,
+    ubicacion2: evento.ubicacion,
+  }));
+
+  const mesesNombres = {
+    '01': 'Enero',
+    '02': 'Febrero',
+    '03': 'Marzo',
+    '04': 'Abril',
+    '05': 'Mayo',
+    '06': 'Junio',
+    '07': 'Julio',
+    '08': 'Agosto',
+    '09': 'Septiembre',
+    '10': 'Octubre',
+    '11': 'Noviembre',
+    '12': 'Diciembre'
+  };
+  
+  const obtenerDiaYMes = (fecha) => {
+    const partesFecha = fecha.split('/'); // Suponiendo que el formato es DD/MM/YY
+    const dia = partesFecha[0];
+    const mesNumero = partesFecha[1];
+    const mesNombre = mesesNombres[mesNumero];
+    
+    return { dia, mes: mesNombre };
+  };
+
+
+
+  
+        return (
+          <div className="row">
+            <div className="col-md-3 col-sm-6 col-xs-12">
+              <SideMenu />
+            </div>
+      
+            <div className="col-md-6 a-proximos-eventos mt-5 text-center mx-auto">
+              {eventosExtraidos.map((evento, index) => {
+                const { dia, mes } = obtenerDiaYMes(evento.fecha);
+      
+                return (
+                  <div key={index} className="container">
+                    <div className="row row-striped">
+                      <div className="col-2 text-right">
+                        <h1 className="display-4">
+                          <span className="badge badge-secondary ">{dia}</span>
+                        </h1>
+                        <h2>{mes}</h2>
+                      </div>
+                      <div className="col-10">
+                        <h3 className="text-uppercase"><strong>Actos</strong></h3>
+                        <h5>{evento.descripcion}</h5>
+                        <p>{evento.ubicacion}</p>
+   
+                   
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+
+
+{eventosExtraidos2.map((evento, index) => {
+                const { dia, mes } = obtenerDiaYMes(evento.fecha2);
+      
+                return (
+                  <div key={index} className="container">
+                    <div className="row row-striped">
+                      <div className="col-2 text-right">
+                        <h1 className="display-4">
+                          <span className="badge badge-secondary">{dia}</span>
+                        </h1>
+                        <h2>{mes}</h2>
+                      </div>
+                      <div className="col-10">
+                        <h3 className="text-uppercase"><strong>Eventos</strong></h3>
+                        <h5>{evento.descripcion2}</h5>
+                        <p>{evento.ubicacion2}</p>
+   
+        
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <div className="row justify-content-center w-100">
-            <div className="event">
-              <div className="a-proximos-eventos card1"> {/* Agregar una clase específica para la primera tarjeta */}
-                <div className="card-body-eventos">
-                  <h2 className="card-title-eventos">ACTOS</h2>
-                  <div className="evento" dangerouslySetInnerHTML={htmlProcesadoActos}></div>
-                </div>
-              </div>
-            </div>
-            <div className="event">
-              <div className="a-proximos-eventos card2"> {/* Agregar una clase específica para la segunda tarjeta */}
-                <div className="card-body-eventos">
-                  <h2 className="card-title-eventos">EVENTOS</h2>
-                  <div className="evento" dangerouslySetInnerHTML={htmlProcesadoEventos}></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div></div>
-  );
-};
-
-export default ProximosEventos;
+        );
+      };
+      
+      export default ProximosEventos;
